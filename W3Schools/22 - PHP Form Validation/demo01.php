5ba53707e474a1c13bfed5debc4b6290ea0bef4f
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-07-01 01:49:42 -0500

    Add more form scripts

--- W3Schools/22 - PHP Form Validation/demo01.php
+++ W3Schools/22 - PHP Form Validation/demo01.php
@@ -17,10 +17,10 @@
 
     // buena práctica: ...
     function test_input($data) {
-        $data = trim($data); // ...
-        $data = stripslashes($data); // ...
-        $data = htmlspecialchars($data); // ...
-        return $data; // ...
+        $data = trim($data); // eliminar caracteres innecesarios (espacio adicional, tabulación, nueva línea)
+        $data = stripslashes($data); // eliminar barras invertidas (\)
+        $data = htmlspecialchars($data); // convertir a entidades HTML
+        return $data;
     }
 ?>
 
@@ -28,16 +28,33 @@
 <h2>PHP Form Validation Example</h2>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
     Name: <input type="text" name="name">
+    <br><br>
     E-mail: <input type="text" name="email">
+    <br><br>
     Website: <input type="text" name="website">
+    <br><br>
     Comment: <textarea name="comment" rows="5" cols="40"></textarea>
-    <br><br>    
-    Gender: <input type="radio" name="gender" value="female">Female
-    Gender: <input type="radio" name="gender" value="male">Male
-    Gender: <input type="radio" name="gender" value="other">Other
+    <br><br>     
+    Gender: 
+    <input type="radio" name="gender" value="female">Female
+    <input type="radio" name="gender" value="male">Male
+    <input type="radio" name="gender" value="other">Other
     <br><br>
     <input type="submit" name="submit" value="Submit">
 </form>
+
+<?php 
+    echo "<h2>Your Input:</h2>";
+    echo $name;
+    echo "<br>";
+    echo $email;
+    echo "<br>";
+    echo $website;
+    echo "<br>";
+    echo $comment;
+    echo "<br>";
+    echo $gender;
+?>
 
 <!-- IMPORTANTE: Un poco de teoría de seguridad  -->
 <h2>IMPORTANTE: htmlspecialchars() function</h2>
