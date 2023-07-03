8131934065b191fb42d3a81e83eeb212fec429a8
jesusdominguez2004 <jesusdominguez2004@gmail.com>
2023-07-03 17:27:06 -0500

    file upload w3schools...

--- W3Schools/31 - PHP File Upload/upload.php
+++ W3Schools/31 - PHP File Upload/upload.php
@@ -1,9 +1,61 @@
 <?php 
     // PHP de demo01.php
-    $target_dir = "uploads/";
+    $target_dir = "uploads/"; // Note: You will need to create a new directory called "uploads" in the directory where "upload.php" file resides. The uploaded files will be saved there.
     $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
     $uploadOk = 1;
     $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
     // Check if image file is a actual image or fake image...
+    if (isset($_POST["submit"])) {
+        $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
+        if ($check !== false) {
+            echo "File is an image - " . $check["mime"] . ".";
+            $uploadOk = 1;
+        } else {
+            echo "File is not an image.";
+            $uploadOk = 0;
+        }
+    }
+
+    // Check if file already exists... EN EL SERVIDOR
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+
+    // Check file size... 500000 = 500_000 = 500KB
+    if ($_FILES["fileToUpload"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats...
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error...
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+            echo "The file " . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
+
+    /*
+        Métodos, variables y constantes usados:
+        $_FILES
+        basename(...)
+        strtolower(...)
+        pathinfo(...)
+        PATHINFO_EXTENSION
+        getimagesize(...)
+        file_exists(...)
+        move_uploaded_file(...)
+        htmlspecialchars(...)
+    */
 ?>
